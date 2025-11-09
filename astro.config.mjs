d13d318f36b2d4372c8f592acc85eaa2d7c62c10
nardoz-dev <nardomen96@gmail.com>
2025-11-09 10:40:07 +0000

    FIX: redirect uri for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
+// astro.config.mjs
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
-
   output: 'static',
-  site: ' https://nardoz-dev.github.io',
+  
+  // Configurazioni per GitHub Pages
+  site: 'https://nardoz-dev.github.io', 
   base: '/work-site-1',
   
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
