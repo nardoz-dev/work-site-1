3fcccd0476823e1d7a203f75dda518eebdbaa86d
nardoz-dev <nardomen96@gmail.com>
2025-11-09 10:20:52 +0000

    FIX: astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
 
-  // Sostituisci con il tuo URL
+  output: 'static',
   site: ' https://nardoz-dev.github.io',
-  
-  // Sostituisci con il nome della tua repository
   base: '/work-site-1',
   
   integrations: [react()],
